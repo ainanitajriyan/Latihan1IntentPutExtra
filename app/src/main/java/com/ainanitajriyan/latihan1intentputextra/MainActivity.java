456ed3979276fe836d7c6b654e746de0b10a3ab2
ainanitajriyan <ainani.tajriyan80@gmail.com>
2020-08-20 20:08:48 +0700

    Commit

---
+++ app/src/main/java/com/ainanitajriyan/latihan1intentputextra/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ainanitajriyan.latihan1intentputextra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText name = (EditText) findViewById(R.id.edit_textdata);
+        Button button = (Button) findViewById(R.id.button_submitIntent);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+}
